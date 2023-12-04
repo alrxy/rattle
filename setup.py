5884453b4e1aba89d3c710d27db0a5af86b013fd
georgiypetrov <petrovgeorgiy11@gmail.com>
2023-12-04 15:19:31 +0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='rattle',
+    version='0.1',
+    description='rattle',
+    url='https://github.com/georgiypetrov/rattle',
+    author='xz',
+    author_email='',
+    license='MIT',
+    packages=['rattle'],
+    install_requires=[
+        # List of dependencies, e.g., 'numpy', 'pandas', etc.
+    ],
+    zip_safe=False
+)
